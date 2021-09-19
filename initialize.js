51aec742bfe80840b1b8c9073b1143f74c06f7f5
REVATIRAMAN DURGAPRASAD TIWARI <84346745+RRevatiaman@users.noreply.github.com>
2021-09-19 16:15:14 +0530

    Add files via upload

---
+++ initialize.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDNCWWvJHPAnNQDZCrJLNJ_EGnNJWff--E",
+    authDomain: "blogging-internship.firebaseapp.com",
+    databaseURL: "https://blogging-internship.firebaseio.com",
+    projectId: "blogging-internship",
+    storageBucket: "blogging-internship.appspot.com",
+    messagingSenderId: "281110167803"
+  };
+  firebase.initializeApp(config);
+
